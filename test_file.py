016e834e8b55f69b3f9818359aadc347e1b72cd1
antijun <davidwu361@gmail.com>
2023-03-07 15:28:26 -0500

    update

---
+++ test_file.py
@@ -0,0 +1,36 @@
+import csv
+
+
+class Album:
+    """
+    A class to represent an album.
+    """
+    name: str
+    artist: str
+    genres: list[str]
+    rank: int
+    release: str
+    descriptors: list[str]
+
+    def __init__(self, name, artist, genres, rank, release, descriptors):
+        self.name = name
+        self.artist = artist
+        self.genres = genres
+        self.rank = rank
+        self.release = release
+        self.descriptors = descriptors
+
+
+albums = []
+with open('datasets/rym_clean1.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        name = row[2]
+        artist = row[3]
+        genres = row[6].split(', ')
+        rank = int(row[1])
+        release = row[4]
+        descriptors = row[7].split(', ')
+        album = Album(name, artist, genres, rank, release, descriptors)
+        albums.append(album)
