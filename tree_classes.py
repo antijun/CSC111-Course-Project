1d05b9636ce8f664881eeac57b01acb6d3d361b9
antijun <davidwu361@gmail.com>
2023-03-26 11:11:14 -0400

    update

--- tree_classes.py
+++ tree_classes.py
@@ -2,13 +2,13 @@ from typing import Any, Optional
 from albums_data import Album, albums
 from genres_data import Genre, genres
 
+
 class Tree:
     """Abstract Tree Class"""
-
     _root: Optional[Any]
-    _subtrees: list[Tree]
+    _subtrees: list[Any]
 
-    def __init__(self, root: Optional[Any], subtrees: list[AbstractTree]) -> None:
+    def __init__(self, root: Optional[Any], subtrees: list[Any]) -> None:
         self._root = root
         self._subtrees = subtrees
 
@@ -16,12 +16,34 @@ class Tree:
         """Returns whether this tree is empty."""
         return self._root is None
 
+    @property
+    def subtrees(self):
+        """Returns the subtrees of this tree."""
+        return self._subtrees
+
+    @property
+    def root(self):
+        """Returns the root of this tree."""
+        return self._root
+
 
 class GenreTree(Tree):
     """A tree that represents genres in a hierarchical structure, each subtree is a subgenre of the root genre."""
-    def __init__(self, root: Optional[Genre], subtrees: list[GenreTree]) -> None:
+
+    def __init__(self, root: Optional[Any], subtrees: list[Any]) -> None:
         super().__init__(root, subtrees)
 
 
 class AlbumTree(Tree):
     """A tree that represents albums in a hierarchical structure, each subtree is an album of the root genre."""
+
+    def __init__(self, root: Optional[Any], subtrees: list[Any]) -> None:
+        super().__init__(root, subtrees)
+
+
+# parent_genres = {genre.parent_genre for genre in genres}
+
+
+def create_genre_tree(genre_list: list[Genre]) -> GenreTree:
+    """Creates a GenreTree that contains all the genres in the genres dataset."""
+    parent_genres = {genre.parent_genre for genre in genre_list}
