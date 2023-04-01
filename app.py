5544f5f7d767a5dca5235d0352eb9b8a81bb4252
antijun <davidwu361@gmail.com>
2023-03-31 21:31:21 -0400

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dash import Dash, html, dcc, callback, Output, Input, ctx, State
 import plotly.express as px
 import pandas as pd
-from plot_genre_tree import fig as genre_fig
+from plot_genre_tree import genre_fig
 from igraph import Graph, EdgeSeq
 import plotly.graph_objects as go
 from albums_data import albums
@@ -24,7 +24,7 @@ app.layout = html.Div([
     html.H1(children='Music Recommendation System', style={'textAlign': 'center'}),
     html.Div(children='''This is a web application that recommends music based on the user's preferences.'''),
     html.Button('Get Recommendation', id='rec_button'),
-    html.Button('Get Genre Tree', id='genre_tree_button'),
+    html.Button('Explore Genre Tree', id='genre_tree_button'),
     html.Div(id='rec_output'),
     dcc.Graph(id='tree_plot', figure=blank_fig())
 ])
@@ -43,7 +43,8 @@ def update_page(rec_button, genre_tree_button):
     """
     if "rec_button" == ctx.triggered_id:
         return html.Div([
-            html.H3('Please select an album that you enjoy listening to from the dropdown below:'),
+            html.H3('Please select an album that you enjoy listening to from the dropdown below:',
+                    style={'textAlign': 'center'}),
             dcc.Dropdown(
                 id='album_dropdown',
                 options=[album.name + ' - ' + album.artist for album in albums],
@@ -53,11 +54,16 @@ def update_page(rec_button, genre_tree_button):
 
     elif "genre_tree_button" == ctx.triggered_id:
         return html.Div([
+            html.H1('Genre Tree Free Exploration', style={'textAlign': 'center'}),
+            html.H3('This is an interactive visualization of music genres displayed in a hierachical tree structure.',
+                    style={'textAlign': 'center'}),
+            html.H3("You can freely explore the tree and view a genre's subgenres by clicking on it's node",
+                    style={'textAlign': 'center'}),
             dcc.Graph(figure=genre_fig)
         ])
     else:
         return html.Div([
-            html.H3('Please press a button to get started')
+            html.H3('Please press a button to get started', style={'textAlign': 'center'}),
         ])
 
 
--- plot_genre_tree.py
+++ plot_genre_tree.py
@@ -1,46 +1,73 @@
 import plotly.graph_objects as go
 from igraph import Graph, EdgeSeq
+from genres_data import genres
 
-nr_vertices = 25
-v_label = list(map(str, range(nr_vertices)))
-G = Graph.Tree(nr_vertices, 2)  # 2 stands for children number
-lay = G.layout('rt')
 
-position = {k: lay[k] for k in range(nr_vertices)}
-Y = [lay[k][1] for k in range(nr_vertices)]
-M = max(Y)
+def plot_genre_tree(root_genre):
+    """
+    This function plots the genre tree of the given root genre.
+    """
+    # Create a graph object
+    G = Graph(directed=True)
 
-es = EdgeSeq(G)  # sequence of edges
-E = [e.tuple for e in G.es]  # list of edges
+    # Add the root node
+    G.add_vertex(root_genre.name)
 
-L = len(position)
-Xn = [position[k][0] for k in range(L)]
-Yn = [2 * M - position[k][1] for k in range(L)]
-Xe = []
-Ye = []
-for edge in E:
-    Xe += [position[edge[0]][0], position[edge[1]][0], None]
-    Ye += [2 * M - position[edge[0]][1], 2 * M - position[edge[1]][1], None]
+    # Add the children of the root node
+    for genre in genres:
+        if genre.parent_genre == root_genre.name:
+            G.add_vertex(genre.name)
+            G.add_edge(root_genre.name, genre.name)
 
-labels = v_label
+    # Create a layout for the graph
+    lay = G.layout('rt')
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=Xe,
-                         y=Ye,
-                         mode='lines',
-                         line=dict(color='rgb(210,210,210)', width=1),
-                         hoverinfo='none'
-                         ))
-fig.add_trace(go.Scatter(x=Xn,
-                         y=Yn,
-                         mode='markers',
-                         name='bla',
-                         marker=dict(symbol='circle-dot',
-                                     size=18,
-                                     color='#6175c1',  # '#DB4551',
-                                     line=dict(color='rgb(50,50,50)', width=1)
-                                     ),
-                         text=labels,
-                         hoverinfo='text',
-                         opacity=0.8
-                         ))
+    # Create the labels for the vertices
+    v_label = G.vs['name']
+
+    # Create a list of positions for the vertices
+    position = {k: lay[k] for k in range(len(lay))}
+    Y = [lay[k][1] for k in range(len(lay))]
+    M = max(Y)
+
+    # Create a list of edges
+    es = EdgeSeq(G)  # sequence of edges
+    E = [e.tuple for e in G.es]  # list of edges
+
+    # Create a list
+    L = len(position)
+    Xn = [position[k][0] for k in range(L)]
+    Yn = [2 * M - position[k][1] for k in range(L)]
+    Xe = []
+    Ye = []
+
+    # Create a list of edges
+    for edge in E:
+        Xe += [position[edge[0]][0], position[edge[1]][0], None]
+        Ye += [2 * M - position[edge[0]][1], 2 * M - position[edge[1]][1], None]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=Xe,
+                             y=Ye,
+                             mode='lines',
+                             line=dict(color='rgb(210,210,210)', width=1),
+                             hoverinfo='none'
+                             ))
+    fig.add_trace(go.Scatter(x=Xn,
+                             y=Yn,
+                             text=v_label,
+                             mode='markers+text',
+                             name='bla',
+                             marker=dict(symbol='circle-dot',
+                                         size=18,
+                                         color='#6175c1',  # '#DB4551',
+                                         line=dict(color='rgb(50,50,50)', width=1)
+                                         ),
+                             hoverinfo='text',
+                             opacity=0.8
+                             ))
+
+    return fig
+
+
+genre_fig = plot_genre_tree(genres[436])
