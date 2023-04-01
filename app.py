814c899f3a075afd8cdd38d5146ed801c76db9d3
antijun <davidwu361@gmail.com>
2023-04-01 15:03:31 -0400

    update

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ from genres_data import Genre
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+
+root_genre_stack = []
 
 
 def blank_fig():
@@ -28,8 +30,8 @@ def blank_fig():
 app.layout = html.Div([
     html.H1(children='Music Recommendation System', style={'textAlign': 'center'}),
     html.Div(children='''This is a web application that recommends music based on the user's preferences.'''),
-    html.Button('Get Recommendation', id='rec_button'),
-    html.Button('Explore Genre Tree', id='genre_tree_button'),
+    html.Button('Get Recommendation', id='rec_button', style={'textAlign': 'center'}),
+    html.Button('Explore Genre Tree', id='genre_tree_button', style={'textAlign': 'center'}),
     html.Div(id='rec_output'),
     dcc.Graph(id='tree_plot', figure=blank_fig()),
     html.Div(id='genre_output'),
@@ -41,7 +43,8 @@ app.layout = html.Div([
     Output('tree_plot', 'figure', allow_duplicate=True),
     Input('rec_button', 'n_clicks'),
     Input('genre_tree_button', 'n_clicks'),
-    prevent_initial_call='initial_duplicate'
+    prevent_initial_call='initial_duplicate',
+    suppress_callback_exceptions=True
 )
 def update_page(rec_button, genre_tree_button):
     """
@@ -58,21 +61,24 @@ def update_page(rec_button, genre_tree_button):
                 options=[album.name + ' - ' + album.artist for album in albums],
             ),
             html.Div(id='album_output')
-        ])
+        ]), blank_fig()
 
     elif "genre_tree_button" == ctx.triggered_id:
+        global root_genre_stack
+        root_genre_stack = []
         return html.Div([
             html.H2('Genre Tree Free Exploration', style={'textAlign': 'center'}),
             html.H3('This is an interactive visualization of music genres displayed in a hierachical tree structure.',
                     style={'textAlign': 'center'}),
             html.H3("You can freely explore the tree and view a genre's subgenres by clicking on it's node",
                     style={'textAlign': 'center'}),
+            html.Button('Go Back', id='back_button', style={'textAlign': 'center'}),
         ]), plot_default_genre_tree()
 
     else:
         return html.Div([
             html.H3('Please press a button to get started', style={'textAlign': 'center'}),
-        ])
+        ]), blank_fig()
 
 
 @app.callback(
@@ -85,10 +91,31 @@ def plot_new_tree(clickData):
     This function plots a new genre tree based on the node clicked on the old genre tree.
     """
     if clickData is not None:
-        genre_name = clickData['points'][0]['text']
-        new_root = Genre(genre_name, None)
-        new_fig = plot_genre_tree(new_root)
-        return new_fig
+        if clickData['points'][0]['text'] == 'Genres':
+            return plot_default_genre_tree()
+        else:
+            genre_name = clickData['points'][0]['text']
+            new_root = Genre(genre_name, None)
+            root_genre_stack.append(new_root)
+            new_fig = plot_genre_tree(new_root)
+            return new_fig
+
+
+@app.callback(
+    Output('tree_plot', 'figure', allow_duplicate=True),
+    Input('back_button', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def plot_previous_tree(back_button):
+    """
+    This function plots the previous genre tree, does nothing if it is currently the default tree.
+    """
+    global root_genre_stack
+    if "back_button" == ctx.triggered_id and len(root_genre_stack) > 0:
+        return plot_genre_tree(root_genre_stack.pop())
+    else:
+        return plot_default_genre_tree()
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
